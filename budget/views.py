828230674a144b46d7ca0c345eba2e33eb89c997
Supraja <supraja.prasad23@gmail.com>
2019-02-19 23:21:51 +0530

    add

---
+++ budget/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def project_list(request):
+	return render(request,'budget/project-list.html')
+
+def project_detail(request,project_slug):
+	return render(request,'budget/project-detail.html')
